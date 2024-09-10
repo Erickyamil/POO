11bf0fa21cd40883802c683319afe9eb2dbddfd8
Erick Yamil Rodríguez Castellanos <146466857+Erickyamil@users.noreply.github.com>
2024-09-09 22:27:06 -0600

    Add files via upload
    Codigo de la actividad 1, promedio ponderado.

---
+++ PromedioPonderado.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class PromedioPonderado {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner (System.in);
+
+        System.out.println("//CALCULO DE PROMEDIO PONDERADO//"); //Mensaje de inicio.
+
+        //Declaración de los tres arreglos principales
+        System.out.print("Cuantas materias cursa: ");
+        int tamaño = entrada.nextInt();
+        int [] arrayCreditos = new int[tamaño];
+        double [] arrayCalif = new double[tamaño];
+        double [] arrayTotal = new double[tamaño];
+        
+        guardarCalif(entrada, arrayCalif); //Guardar en un array las calificaciones ingresadas por el usuario.
+        guardarCalif(entrada, arrayCreditos);//Guardar en un array los creditos de cada materia segun el usuario.
+        obtenerTotal(arrayCreditos, arrayCalif, arrayTotal); //Guardar en un array los valores del total (total = creditos*calificaciones).
+        obtenerPromedioPonder(obtenerSuma(arrayTotal), obtenerSuma(arrayCreditos));// Calcular el promedio ponderado final.
+        }
+
+    //Metodos encargados de guardar las calificaciones ingresadas por el usuario.
+    public static void guardarCalif(Scanner entrada, double[] arrayCalif){
+        for(int i=0; i<arrayCalif.length; i++){
+            System.out.print("Ingrese su calificacion " + (i+1) + " : ");
+            arrayCalif[i] = entrada.nextInt();
+        }
+    }
+
+    public static void guardarCalif(Scanner entrada, int[] arrayCreditos){
+        for(int i=0; i<arrayCreditos.length; i++){
+            System.out.print("Ingrese el valor de creditos para la mteria " + (i+1) + " : ");
+            arrayCreditos[i] = entrada.nextInt();
+        }
+    }
+
+    //Metodo encargado de calcular y asignar los valores del total en un arreglo.
+    public static void obtenerTotal(int[] arrayCreditos, double[] arrayCalif, double[] arrayTotal){
+        for(int i=0; i<6; i++){
+            arrayTotal[i] = (arrayCreditos[i] * arrayCalif[i]);
+        }
+    }
+
+    //Metodos encargados de sumar todos los valores de un arreglo.
+    public static double obtenerSuma(int[] array){
+        double suma = 0;
+        for(int i=0; i<array.length; i++){
+            suma += array[i];
+        }
+        return suma;
+    }
+
+    public static double obtenerSuma(double [] array){
+        double suma = 0;
+        for(int i=0; i<array.length; i++){
+            suma += array[i];
+        }
+        return suma;
+    }
+
+    //Metodo encargado de imprimir un arreglo.
+    public static void imprimirArray(int[] array){
+        for(int i=0; i<array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+
+    //Metodo encargado de obtener el promedio ponderado final.
+    public static void obtenerPromedioPonder(double totalT, double creditosT){
+        double promedioP = (totalT/creditosT);
+        System.out.printf("Promedio Ponderado = %.2f%n", promedioP);
+    }
+}
